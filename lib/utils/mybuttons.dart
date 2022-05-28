46e2f9c2d90555caac1379b2c6e8e6654cd56810
Gideon Okoroafor <okoro062@umn.edu>
2022-05-28 14:22:21 -0500

    my buttons class

---
+++ lib/utils/mybuttons.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String label;
+  final Function() onTap;
+  final double width;
+  final double height;
+  final Color color;
+
+  const MyButton({Key? key,
+    required this.label,
+    required this.onTap,
+    required this.height,
+    required this.width,
+    required this.color
+  })
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: width,
+        height: height,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: color,
+        ),
+        child: Text(
+          label,
+          style: TextStyle(fontSize: 15.0, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
