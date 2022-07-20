cb567d1bc7a49465fb96d7e70838c8368efcabe1
Gideon Okoroafor <okoro062@umn.edu>
2022-07-20 08:46:46 -0500

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
-import 'package:sfree/views/user_login.dart';
-import 'package:sfree/views/welcome.dart';
+import 'package:sfree/authentication/sign_in_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const Welcome());
-  // runApp(const UserLogin());
+  // runApp(const Welcome());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignInAuth(),
+    );
+  }
 }
